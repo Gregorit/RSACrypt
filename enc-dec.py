ec80fd14314c4544686f8c98006309e142d5e66b
Gregorit <grzegorz.urych@gmail.com>
2019-06-01 18:27:35 +0200

    Add public key generator

--- enc-dec.py
+++ enc-dec.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def prime_check(n):
     if n == 2:
@@ -14,30 +15,44 @@ def prime_check(n):
 
 def prime_gen(choice):
     min_number = 2
-    max_number = 1000
+    max_number = 10000
     products = []
-    genetating = True
+    generating = True
 
-    while genetating:
+    while generating:
         if choice == 'manual':
             if len(products) < 1:
                 number_gen = int(input('Provide first prime number: '))
             elif len(products) < 2:
                 number_gen = int(input('Provide second prime number: '))
-
         elif choice == 'auto':    
             number_gen = random.randint(min_number, max_number)
 
         if prime_check(number_gen):
             products.append(number_gen)
-        elif choice == 1:
+        elif choice == 'manual':
             print(f'\n|| {number_gen} is not a prime number.\n'
                    '|| Try again.\n')
 
         if len(products) == 2:
-            genetating = False
+            generating = False
     return products
 
+
+def key_gen(p, q):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    # Generating e which will be relatively prime to phi
+    while True:
+        e = random.randrange(1, phi)
+        if math.gcd(e, phi) == 1:
+            break
+
+
+    #put here method for private key
+    
+    return((n, e))
 
 
 choice = input('Manual (1) or Auto (anything else): ')
@@ -49,3 +64,6 @@ else:
 p, q = prime_gen(choice)
 print(f'p: {p}\n'
       f'q: {q}')
+
+public_key = key_gen(p, q)
+print(f'Public key:\n{public_key}')
