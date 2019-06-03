ff2e448a57bc2a6019e65dcf1327656f0d84fcde
Gregorit <grzegorz.urych@gmail.com>
2019-06-03 09:34:57 +0200

    Add key, cryption and inversion definitions

--- enc-dec.py
+++ enc-dec.py
@@ -1,7 +1,10 @@
 import random
 import math
+import sys
+
 
 def prime_check(n):
+    '''Function checks if provided number is prime'''
     if n == 2:
         return True
     if n % 2 == 0 or n < 2:
@@ -14,12 +17,14 @@ def prime_check(n):
 
 
 def prime_gen(choice):
+    '''Function generates prime numbers with help of prime_check.
+    There's two options for those numbers: manual or automatic providing.
+    '''
     min_number = 2
     max_number = 10000
     products = []
-    generating = True
 
-    while generating:
+    while True:
         if choice == 'manual':
             if len(products) < 1:
                 number_gen = int(input('Provide first prime number: '))
@@ -35,11 +40,23 @@ def prime_gen(choice):
                    '|| Try again.\n')
 
         if len(products) == 2:
-            generating = False
+            if products[0] == products[1]:
+                products = []
+                continue
+            break
     return products
 
 
+def multiplicative_inverse(a, b):
+    '''Function makes multiplicative inversion by using generated e and phi'''
+    for lx in range(0, b):
+        if (a*lx) % b == 1:
+            return lx
+    sys.exit(-1)
+
+
 def key_gen(p, q):
+    '''Function generates public and private keys using provided primes'''
     n = p * q
     phi = (p - 1) * (q - 1)
 
@@ -49,10 +66,28 @@ def key_gen(p, q):
         if math.gcd(e, phi) == 1:
             break
 
+    d = multiplicative_inverse(e, phi)
 
-    #put here method for private key
-    
-    return((n, e))
+    return((n, e), (n, d))
+
+
+def encrypt(private, plaintext):
+    '''Encryption with previously generated private key'''
+    n, key = private
+    # Generator which converts each letter in the plaintext
+    # to numbers based on the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+
+    return cipher
+
+def decrypt(public, ciphertext):
+    '''Decryption with previously generated public key'''
+    n, key = public
+    # Generator which generate the plaintext based
+    # on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+
+    return ''.join(plain)
 
 
 choice = input('Manual (1) or Auto (anything else): ')
@@ -61,9 +96,20 @@ if choice == '1':
 else:
     choice = 'auto'
 
-p, q = prime_gen(choice)
-print(f'p: {p}\n'
-      f'q: {q}')
+first, second = prime_gen(choice)
+print(f'First prime:  {first}\n'
+      f'Second prime: {second}')
 
-public_key = key_gen(p, q)
-print(f'Public key:\n{public_key}')
+public_key, private_key = key_gen(first, second)
+print(f'--- Public key ---\n{public_key}\n'
+      f'\n--- Private key ---\n{private_key}\n')
+
+text = input("Type message: ")
+
+enc_text = encrypt(private_key, text)
+print('\n*** Encrypted message ***')
+print(''.join(map(lambda x: str(x), enc_text)))
+dec_text = decrypt(public_key, enc_text)
+print('\n*** Decrypted message ***\n'
+      f'{dec_text}')
+sys.exit(0)
